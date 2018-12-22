cf7cbc6494febe9494796619bb5cd5a3e6b764d7
root <erpnextdeveloper1@gmail.com>
2018-12-22 08:27:34 +0000

    doctypes

---
+++ sahidaam/sahidaam/doctype/condition_parameter/condition_parameter.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, Crisco Consulting and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Condition Parameter', {
+	refresh: function(frm) {
+
+	}
+});
---
+++ sahidaam/sahidaam/doctype/custom_error_log/custom_error_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, Crisco Consulting and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Custom Error Log', {
+	refresh: function(frm) {
+
+	}
+});
